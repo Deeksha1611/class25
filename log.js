5b94eef1e25894647663d9115327645877128f33
Deeksha1611 <74140836+Deeksha1611@users.noreply.github.com>
2020-11-25 17:51:39 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,6 @@
+class LOG extends BASECLASS{
+    constructor(x,y,h,a){
+        super(x,y,20,h,a);
+        this.image=loadImage("sprites/wood2.png")
+        Matter.Body.setAngle(this.body,a);
+    }}
